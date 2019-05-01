5d7877ad78a0ff1c2a4b406a751aa6a6199ab961
Cornelius Zhu <hongzhi.zhu@outlook.com>
2019-04-30 23:00:35 -0700

    Annotated files

--- NPD_v5/hzhu_npd.h
+++ NPD_v5/hzhu_npd.h
@@ -675,16 +675,16 @@ hzhu_mat hzhu_npd_results(hzhu_mat &detect, hzhu_mat &result_X, hzhu_mat &result
 		hzhu_mat tmp = result_X.get_sub(a, 7, b, 7);
 		int max_index = tmp.min_index_all().data->data[0];
 		re.data->data[j * 10 + 2] = max_index + a;//index of start point t1 for X (determined by the best fitted data section)
-		re.data->data[j * 10 + 3] = result_X.data->data[(max_index + a) * N];//theta1 for X
-		re.data->data[j * 10 + 4] = result_X.data->data[(max_index + a) * N + 1];//theta2 for X
-		re.data->data[j * 10 + 5] = result_X.data->data[(max_index + a) * N + 2];//theta3 for X
+		re.data->data[j * 10 + 3] = result_X.data->data[(max_index + a) * N];//best fitted theta1 for X
+		re.data->data[j * 10 + 4] = result_X.data->data[(max_index + a) * N + 1];//best fitted theta2 for X
+		re.data->data[j * 10 + 5] = result_X.data->data[(max_index + a) * N + 2];//best fitted theta3 for X
 
 		hzhu_mat Tmp = result_Y.get_sub(a, 7, b, 7);
 		max_index = Tmp.min_index_all().data->data[0];
 		re.data->data[j * 10 + 6] = max_index + a;//index of start point t1 for Y (determined by the best fitted data section)
-		re.data->data[j * 10 + 7] = result_Y.data->data[(max_index + a) * N];//theta1 for Y
-		re.data->data[j * 10 + 8] = result_Y.data->data[(max_index + a) * N + 1];//theta2 for Y
-		re.data->data[j * 10 + 9] = result_Y.data->data[(max_index + a) * N + 2];//theta3 for Y
+		re.data->data[j * 10 + 7] = result_Y.data->data[(max_index + a) * N];//best fitted theta1 for Y
+		re.data->data[j * 10 + 8] = result_Y.data->data[(max_index + a) * N + 1];//best fitted theta2 for Y
+		re.data->data[j * 10 + 9] = result_Y.data->data[(max_index + a) * N + 2];//best fitted theta3 for Y
 	}
 
 	return re;
